116ab2e36812f4ee1b1646e6f3408f99588d14d9
EAS7510 <michaeleaster@northwesternmutual.com>
2019-09-17 18:09:27 -0500

    Add assertRecordsMatch overloads with ignored fields.

--- src/main/java/fun/mike/record/RecordAssert.java
+++ src/main/java/fun/mike/record/RecordAssert.java
@@ -1,5 +1,6 @@
 package fun.mike.record;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public class RecordAssert {
@@ -12,8 +13,26 @@ public class RecordAssert {
     public static void assertRecordsMatch(Record expected, Record actual) {
         Optional<RecordDiffer.Diff> optionalDiff = RecordDiffer.diff(expected, actual);
 
-        optionalDiff.ifPresent(diff1 -> {
-            RecordDiffer.Diff diff = diff1;
+        optionalDiff.ifPresent(diff -> {
+            throw new AssertionError(RecordDiffer.describe(diff));
+        });
+    }
+
+    /**
+     * Asserts that two records are equal. If they are not, an
+     * {@link AssertionError} without a message is thrown.
+     * @param expected expected record
+     * @param actual the record to check against <code>expected</code>
+     * @param ignoredKeys keys to ignore
+     */
+    public static void assertRecordsMatch(Record expected,
+                                          Record actual,
+                                          Collection<String> ignoredKeys) {
+        Optional<RecordDiffer.Diff> optionalDiff =
+                RecordDiffer.diff(expected.dissoc(ignoredKeys),
+                                  actual.dissoc(ignoredKeys));
+
+        optionalDiff.ifPresent(diff -> {
             throw new AssertionError(RecordDiffer.describe(diff));
         });
     }
@@ -28,8 +47,29 @@ public class RecordAssert {
     public static void assertRecordsMatch(String message, Record expected, Record actual) {
         Optional<RecordDiffer.Diff> optionalDiff = RecordDiffer.diff(expected, actual);
 
-        optionalDiff.ifPresent(diff1 -> {
-            RecordDiffer.Diff diff = diff1;
+        optionalDiff.ifPresent(diff -> {
+            throw new AssertionError(RecordDiffer.describe(diff, message));
+        });
+    }
+
+    /**
+     * Asserts that two records are equal, ignoring the given set of keys. If
+     * they are not, an {@link AssertionError} is thrown with the given
+     * message.
+     * @param message the identifying message for the {@link AssertionError}
+     * @param expected expected record
+     * @param actual the record to check against <code>expected</code>
+     * @param ignoredKeys keys to ignore
+     */
+    public static void assertRecordsMatch(String message,
+                                          Record expected,
+                                          Record actual,
+                                          Collection<String> ignoredKeys) {
+        Optional<RecordDiffer.Diff> optionalDiff =
+                RecordDiffer.diff(expected.dissoc(ignoredKeys),
+                                  actual.dissoc(ignoredKeys));
+
+        optionalDiff.ifPresent(diff -> {
             throw new AssertionError(RecordDiffer.describe(diff, message));
         });
     }
